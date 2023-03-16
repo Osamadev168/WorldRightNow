a5675a12943181f834010b7a2da2183bde9f72f1
Daniyal-Habib <70368916+Daniyal-Habib@users.noreply.github.com>
2023-03-16 21:10:04 +0500

    Contact us page create and some other things

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -8,25 +8,20 @@ const Banner = () => {
         <div className="mainContainerBanner">
           <div className="bannerdiv1">
             <h1 className="text1banner">
-              Unlock Your <br />
-              Business's Potential with Our Blog Posting
+              <span>Hubble Feed</span> - Blog posting of the future
             </h1>
             <p className="text2banner">
               Easily grow your business and reach new audiences by posting your
-              blogs on our website. With our SEO optimisation and targeted
+              blogs on Hubble Feed. With our SEO optimisation and targeted
               traffic, your business will get the online exposure it needs to
               thrive. Contact us & Start blogging your way to success now!
             </p>
           </div>
           <div className="bannerdiv2">
-            <a className="primaryButton" href="/blogs">
+            <a className="primaryButton" href="/contact">
               Contact us
             </a>
-            <a
-              className="secondaryButton"
-              // onClick={() => window.open("/all/blogs", "_self")}
-              href="/all/blogs"
-            >
+            <a className="secondaryButton" href="/all/blogs">
               Explore Blogs
             </a>
           </div>
