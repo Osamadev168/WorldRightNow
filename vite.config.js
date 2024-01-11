8f641f97b0be3d605dcf98e40e28071663c4e68b
Osama Habib <84494988+Osamadev168@users.noreply.github.com>
2024-01-11 16:45:46 +0500

    sourcemap issue resolved

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions: {
       external: ["ckeditor5-custom-build"],
       output: {
